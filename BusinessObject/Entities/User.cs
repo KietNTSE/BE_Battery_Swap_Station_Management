ddbe24b4558feaede0ee1504271b0e4ed9641793
KietNTSE <kietnguyen14022005@gmail.com>
2025-10-01 08:47:07 +0700

    chore: configure dotnet tools and update database connection settings

--- BusinessObject/Entities/User.cs
+++ BusinessObject/Entities/User.cs
@@ -1,6 +1,6 @@
-﻿using BusinessObject.Enums;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using BusinessObject.Enums;
 
 namespace BusinessObject.Entities
 {
