13e22eabeb8b400cdde1cdd47bddab8210ddd6e2
PhatDylan <phatnguyentan2205@gmail.com>
2025-10-06 10:57:28 +0700

    Dto Update 10/6

--- BusinessObject/Dtos/BookingDto.cs
+++ BusinessObject/Dtos/BookingDto.cs
@@ -11,7 +11,6 @@ public class BookingBase
     public string VehicleId { get; set; } = string.Empty;
     public string BatteryId { get; set; } = string.Empty;
     public string BatteryTypeId { get; set; } = string.Empty;
-    public DateTime BookingDate { get; set; }
     public string TimeSlot { get; set; } = string.Empty;
     public BBRStatus Status { get; set; }
     public DateTime CreatedAt { get; set; }
